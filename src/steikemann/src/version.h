c61b21fd9bd238709c965016787cf1e64ce2cad1
adrianrs_goetia <adrian.r.strate@gmail.com>
2025-06-06 23:51:50 +0200

    cmake compilation. precompile godot-cpp debug binaries for linux and windows locally.

--- src/steikemann/src/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define STEIKEMANN_MAJOR 0
-#define STEIKEMANN_MINOR 0
-#define STEIKEMANN_PATCH 3
-
-#define _STEIKEMANN_VERSION_STR_TEXT(text) #text
-#define _STEIKEMANN_VERSION_STR(major, minor, patch)                                                                   \
-	_STEIKEMANN_VERSION_STR_TEXT(major) "." _STEIKEMANN_VERSION_STR_TEXT(minor) "." _STEIKEMANN_VERSION_STR_TEXT(patch)
-#define STEIKEMANN_VERSION_STR _STEIKEMANN_VERSION_STR(STEIKEMANN_MAJOR, STEIKEMANN_MINOR, STEIKEMANN_PATCH)
