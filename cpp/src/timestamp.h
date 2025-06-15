7558319da068c72e50621a3708cf5c2519e2d3d0
adrianrs_goetia <adrian.r.strate@gmail.com>
2025-06-15 17:14:37 +0200

    Timestamp::time_since_stamp method

--- cpp/src/timestamp.h
+++ cpp/src/timestamp.h
@@ -10,11 +10,15 @@ public:
 		m_timestamp = std::chrono::steady_clock::now();
 	}
 
-	auto in_range(float t_time_range_in_seconds) -> bool {
+	auto time_since_stamp() -> float {
 		float duration_since_timestamp
 			= std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - m_timestamp)
 				  .count();
-		float sec = (duration_since_timestamp / 1e9);
+		return (duration_since_timestamp / 1e9);
+	}
+
+	auto in_range(float t_time_range_in_seconds) -> bool {
+		auto duration_since_timestamp = time_since_stamp();
 		return (duration_since_timestamp / 1e9) < t_time_range_in_seconds;
 	}
 };
