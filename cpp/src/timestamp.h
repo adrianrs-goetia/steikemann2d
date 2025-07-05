1d0295bc5fec486ad6d9f52561106aebb9b070e5
adrianrs_goetia <adrian.r.strate@gmail.com>
2025-07-05 19:04:05 +0200

    system_clock instead of steady_clock

--- cpp/src/timestamp.h
+++ cpp/src/timestamp.h
@@ -3,16 +3,16 @@
 #include <chrono>
 
 class TimeStamp {
-	std::chrono::steady_clock::time_point m_timestamp;
+	std::chrono::system_clock::time_point m_timestamp;
 
 public:
 	auto stamp() -> void {
-		m_timestamp = std::chrono::steady_clock::now();
+		m_timestamp = std::chrono::system_clock::now();
 	}
 
 	auto time_since_stamp() -> float {
 		const auto duration_since_timestamp
-			= std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - m_timestamp)
+			= std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now() - m_timestamp)
 				  .count();
 		return (static_cast<float>(duration_since_timestamp) / 1e9);
 	}
